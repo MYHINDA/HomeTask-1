5b54a1030215b0c0819fb0dc03c413cdefd9651f
hinda <h8565956@gmail.com>
2023-05-10 21:39:34 +0300

    some updates for conditions in triangle mode

--- twitter.py
+++ twitter.py
@@ -7,17 +7,19 @@ def tower():
 
     if choise < 0 or choise > 3:
         return"Invalid Input"
+    
     def h_w():
         height_width = input("Please enter the height and width  ")
         height = int(height_width.split(' ')[0])
         width = int(height_width.split(' ')[1])
         return height, width
-    height,width = h_w()
-    # while choise!=
+    
+
     while choise <= 3:
         match(choise):
 
             case 1:
+                height, width = h_w()
                 print("You chose a rectangular tower ")
                 if height == width or abs(height - width) > 5:
                     print("The area of the rectangle is ", height*width)
@@ -25,12 +27,12 @@ def tower():
                     print("The perimeter of the rectangle is " , 2*(height+width))
 
             case 2:
+                height, width = h_w()
                 print("You chose a triangular tower")
                 
                 triangle_choise = int(input("To calculate the perimeter of the triangle press 1 \
                                         To print the triangle press 2 "))
-                # height = int(height_width.split(' ')[0])
-                # width = int(height_width.split(' ')[1])
+                
 
                 if triangle_choise == 1:
                     per = (pow((height**2 + int(width/2)**2),0.5))*2 + width
@@ -38,10 +40,11 @@ def tower():
 
                 elif triangle_choise == 2:
 
-                    if width % 2 == 0 or (width*2 > height):
-                        print("This triangle cannot be printed")
-                    else:
+                    if  width % 2 != 0 and height*2 > width:
                         ptriangle(height, width)
+                        
+                    else:
+                        print("This triangle cannot be printed")
             case 3:
                 return "good bye"
             
@@ -49,5 +52,5 @@ def tower():
         choise = int(input("Enter your choise "))
         if choise < 0 or choise > 3:
             return "Invalid Input"
-        height, width = h_w()
+        
 print(tower())

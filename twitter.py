b05c6137584751868db3d5e7151c65be334401d8
hinda <h8565956@gmail.com>
2023-05-10 21:16:41 +0300

    fix perimeter of triangle

--- twitter.py
+++ twitter.py
@@ -7,35 +7,36 @@ def tower():
 
     if choise < 0 or choise > 3:
         return"Invalid Input"
-    
-    height_width = input("Please enter the height and width  ")
-    height = int(height_width.split(' ')[0])
-    width = int(height_width.split(' ')[1])
-
+    def h_w():
+        height_width = input("Please enter the height and width  ")
+        height = int(height_width.split(' ')[0])
+        width = int(height_width.split(' ')[1])
+        return height, width
+    height,width = h_w()
     # while choise!=
     while choise <= 3:
         match(choise):
 
             case 1:
                 print("You chose a rectangular tower ")
-                height_width
                 if height == width or abs(height - width) > 5:
                     print("The area of the rectangle is ", height*width)
-                print("The perimeter of the rectangle is " , 2*(height+width))
+                else:
+                    print("The perimeter of the rectangle is " , 2*(height+width))
 
             case 2:
                 print("You chose a triangular tower")
-                height_width
-                triangle_chise = int(input("To calculate the perimeter of the triangle press 1 \
+                
+                triangle_choise = int(input("To calculate the perimeter of the triangle press 1 \
                                         To print the triangle press 2 "))
-                height = int(height_width.split(' ')[0])
-                width = int(height_width.split(' ')[1])
+                # height = int(height_width.split(' ')[0])
+                # width = int(height_width.split(' ')[1])
 
-                if triangle_chise == 1:
-                    per = pow((height**2 - width**2),0.5)
-                    print("The perimeter of the rectangle is ", per)
+                if triangle_choise == 1:
+                    per = (pow((height**2 + int(width/2)**2),0.5))*2 + width
+                    print("The perimeter of the triangle is ", per)
 
-                elif triangle_chise == 2:
+                elif triangle_choise == 2:
 
                     if width % 2 == 0 or (width*2 > height):
                         print("This triangle cannot be printed")
@@ -48,5 +49,5 @@ def tower():
         choise = int(input("Enter your choise "))
         if choise < 0 or choise > 3:
             return "Invalid Input"
-
+        height, width = h_w()
 print(tower())
